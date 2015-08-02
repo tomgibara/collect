0b63dd0c8df09bca668565c00ae8e9bc3f0d7125
Tom Gibara <me@tomgibara.com>
2015-08-02 12:33:28 +0100

    Introduces StoreIterator.

---
+++ src/main/java/com/tomgibara/collect/StoreIterator.java
@@ -0,0 +1,49 @@
+package com.tomgibara.collect;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+abstract class StoreIterator<V, E> implements Iterator<E> {
+
+	private final Store<V> store;
+	private int previous = -1;
+	private int next = subsequent(previous + 1);
+
+	StoreIterator(Store<V> store) {
+		this.store = store;
+	}
+	
+	@Override
+	public E next() {
+		if (next == store.capacity()) throw new NoSuchElementException();
+		E x = get(next);
+		int tmp = next;
+		next = subsequent(previous + 1);
+		previous = tmp;
+		return x;
+	}
+
+	@Override
+	public boolean hasNext() {
+		return next != store.capacity();
+	}
+
+	@Override
+	public void remove() {
+		if (previous == -1) throw new NoSuchElementException();
+		V value = store.get(previous);
+		if (value == null) throw new IllegalStateException();
+		store.set(previous, null);
+	}
+	
+	abstract E get(int index);
+
+	private int subsequent(int i) {
+		int length = store.capacity();
+		for (; i < length; i++) {
+			if (store.get(i) != null) return i;
+		}
+		return length;
+	}
+
+}
